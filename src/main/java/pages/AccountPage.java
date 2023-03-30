23bbcf8d556ec2c40e31c33b0290d07e7f4b8e86
Eduard Bagautdinov <edbag88@gmail.com>
2023-03-30 19:08:43 +0500

    add classes AccountPage, RecoverPassword

---
+++ src/main/java/pages/AccountPage.java
@@ -0,0 +1,16 @@
+package pages;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import static com.codeborne.selenide.WebDriverRunner.url;
+public class AccountPage extends MainPage {
+    @FindBy(how = How.XPATH, using = ".//button[text()='Оформить заказ']")
+    private SelenideElement createOrderBtn;
+    @FindBy(how = How.XPATH, using = ".//a[@href='/account']")
+    private SelenideElement personalAccountBtn;
+    public boolean isUrlCorrectAfterLogin(){
+        createOrderBtn.shouldBe(Condition.visible);
+        return url().equals(MAIN_PAGE_URL);
+    }
+}
